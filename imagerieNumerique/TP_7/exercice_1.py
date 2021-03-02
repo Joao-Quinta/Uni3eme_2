ad4715a44fcd30e426394b4b15832cd90a65de0d
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-03-02 08:14:52 +0100

    s

--- imagerieNumerique/TP_7/exercice_1.py
+++ imagerieNumerique/TP_7/exercice_1.py
@@ -1,3 +1,36 @@
 import lib
 
-lib.test()
+# a
+
+t = lib.returnLinspace(-10, 10, 201)
+print(len(t))
+y1 = lib.computeSha(t, 1)
+y05 = lib.computeSha(t, 0.5)
+y2 = lib.computeSha(t, 2)
+
+# lib.plotScatter([[t, y1, '#1f77b4']])
+# lib.plotScatter([[t, y05, '#1f77b4']])
+# lib.plotScatter([[t, y2, '#1f77b4']])
+
+# b
+
+# lets assume T = 1, we want to sample the f(x) = y values such that x is not float
+# since sha(1) is equal to 1 on all the x values we want, and 0 on floats, we simply multiply and get the same result
+
+# c
+
+tb = t.copy()
+tb1 = t.copy()
+tb2 = t.copy()
+tb05 = t.copy()
+for i in range(0, len(tb)):
+    tb[i] = lib.math.sin(tb[i] + (lib.math.pi / 4))
+    tb1[i] = tb[i] * y1[i]
+    tb05[i] = tb[i] * y05[i]
+    tb2[i] = tb[i] * y2[i]
+
+# lib.plotScatter([[t, tb, '#1f77b4'], [t, tb05, '#ff7f0e']])
+# lib.plotScatter([[t, tb, '#1f77b4'], [t, tb1, '#ff7f0e']])
+# lib.plotScatter([[t, tb, '#1f77b4'], [t, tb2, '#ff7f0e']])
+
+# d
--- imagerieNumerique/TP_7/lib.py
+++ imagerieNumerique/TP_7/lib.py
@@ -1,3 +1,31 @@
-import skimage as ski
-def test():
-    print("test1")
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def returnLinspace(l, r, n):
+    return np.linspace(l, r, n)
+
+
+def computeSha(t, T):
+    y = []
+    for i in t:
+        yi = 0
+        if (i * T).is_integer():
+            yi = 1
+        y.append(yi)
+    return y
+
+
+def plotScatter(functions):
+    for func in functions:
+        plt.scatter(func[0], func[1], c=func[2])
+    plt.show()
+
+
+def even(s):
+    return
+
+
+def odd(s):
+    return
