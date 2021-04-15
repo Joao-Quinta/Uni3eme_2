cff92584737a10bfa20efc96528122c0e52809e1
Joao Filipe Costa Da Quinta <qjoao.filipe@gmail.com>
2021-04-15 10:12:17 +0200

    walla

--- cloo/tp_6/src/test/java/RPGTest.java
+++ cloo/tp_6/src/test/java/RPGTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 //@TestInstance(TestInstance.Lifecycle.PER_METHOD);
 
-class Testee {
+class RPGTest {
 
-    
+
     @BeforeAll
     static void setUpAll(){
         System.out.println("BeforeAll test");
@@ -27,16 +27,16 @@ class Testee {
         System.out.println("AfterALl test");
     }
 
-    
+
 
     @Test
     void getMagicalTest(){
         System.out.println("Test Damage getter method for magical damages.");
         Called t = new Called(4);
         assertEquals(4,t.getNumber());
-        
+
         //Damage dmg = new Damage(0,120,0,0);
-        
+
         //assertEquals(120,dmg.getMagical());
     }
 
