303aada4d72b32cbcce33dd8be443a569acf4369
Joao Filipe Costa Da Quinta <qjoao.filipe@gmail.com>
2021-04-15 11:17:03 +0200

    asf

--- cloo/gradedTP_skeleton/src/main/java/ch/unige/cui/rpg/Game.java
+++ cloo/gradedTP_skeleton/src/main/java/ch/unige/cui/rpg/Game.java
@@ -3,8 +3,5 @@ package ch.unige.cui.rpg;
 public class Game{
 	public static void main(String[] args){
 		System.out.println("Successfully entered in main.");
-		CharProfile mageProfile = new CharProfile(1,0,0,0,0);
-		MageClass mage = new MageClass("mage", 10, 10, new ProtectionStack(null, null),mageProfile);
-		mage.levelUp(mageProfile);
 	}
 }
