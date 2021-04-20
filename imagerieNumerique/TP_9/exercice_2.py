63e5ece5c74145930df90301bce243fecf378a20
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-04-21 00:51:09 +0200

    f

--- imagerieNumerique/TP_9/exercice_2.py
+++ imagerieNumerique/TP_9/exercice_2.py
@@ -1,1 +1,16 @@
 import lib
+import numpy as np
+
+n = 400
+x = np.linspace(-10, 10, n)
+
+gaussianSignal_y = np.zeros(n)
+gaussianTransform_y = np.zeros(n)
+
+signalSinusSqr = np.sinc(x) ** 2
+
+for i in range(len(x)):
+    gaussianSignal_y[i] = lib.gaussianSignal(x[i], 1)
+    gaussianTransform_y[i] = lib.gaussianTransform(gaussianSignal_y[i], 1)
+
+lib.affichage_rows_cols(2, 2, [x, x, x, x], [gaussianSignal_y, gaussianTransform_y, gaussianSignal_y, gaussianSignal_y], ["g(t,a) -> a = 1", "G(w, a)", "F(w)", "G(w) * F(w)"])
--- imagerieNumerique/TP_9/lib.py
+++ imagerieNumerique/TP_9/lib.py
@@ -1,3 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+
+def gaussianSignal(t, sigma):
+    return np.exp(-(t ** 2) / (2 * sigma ** 2)) / np.sqrt(2 * np.pi * sigma ** 2)
+
+
+def gaussianTransform(gaussianS, sigma):
+    return gaussianS * (np.sqrt(2 * np.pi) / sigma)
+
+
+def simplePlot(x, y):
+    plt.plot(x, y)
+    plt.show()
+
+
+def affichage_rows_cols(rows, cols, x, y, labels):
+    rows = rows
+    cols = cols
+    axes = []
+    fig = plt.figure()
+    for i in range(rows * cols):
+        axes.append(fig.add_subplot(rows, cols, i + 1))
+        plt.plot(x[i], y[i])
+        plt.title(labels[i])
+    fig.tight_layout()
+    plt.show()
