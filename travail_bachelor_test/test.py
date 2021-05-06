5440c16d0799a8b2df1ae3fb9b73cc05cd06bad3
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-05-06 03:00:14 +0200

    wa

--- travail_bachelor_test/test.py
+++ travail_bachelor_test/test.py
@@ -1,10 +1,11 @@
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+a = []
+
+for val in range(100):
+    a.append(str(val))
+
+print(a)
+val = 90
+print(a[val + 0] + " " + a[val + 1] + " " + a[val + 2] + " " + a[val + 3] + " " + a[val + 4] + " " + a[
+    val + 5] + " " + a[val + 6] + " " + a[val + 7] + " " + a[val + 8] + " " + a[val + 9])
 
 
-print(fib(38))
