42d5af39a1ff06cd0459cfd3357e7d02a1ec4f0e
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-05-18 19:02:50 +0200

    exo1

---
+++ imagerieNumerique/TP_11/exercice_1.py
@@ -0,0 +1,66 @@
+import numpy as np
+from skimage import transform
+import lib
+
+# (a)
+intervalStart = 1
+intervalEnd = 256
+n = 256
+
+one_axe = np.linspace(intervalStart, intervalEnd, n)
+x_axis, y_axis = np.meshgrid(one_axe, one_axe)
+
+image_1_a = np.sin(2 * np.pi * x_axis / (n / 10))
+dft_image_1_a = np.fft.fftshift(image_1_a)
+magnitude_dft_image_1_a = np.abs(dft_image_1_a)
+
+imagesToPlot = [image_1_a, np.log(magnitude_dft_image_1_a + 1)]
+label = ["sinusoidal vertical", "magnitude"]
+
+lib.affichage_rows_cols_images(1, 2, imagesToPlot, label)
+
+# (b)
+
+image_1_b = np.sin(2 * np.pi * y_axis / (n / 5))
+dft_image_1_b = np.fft.fftshift(image_1_b)
+magnitude_dft_image_1_b = np.abs(dft_image_1_b)
+
+imagesToPlot = [image_1_b, np.log(magnitude_dft_image_1_b + 1)]
+label = ["sinusoidal horizontal", "magnitude"]
+
+lib.affichage_rows_cols_images(1, 2, imagesToPlot, label)
+
+# (c)
+
+image_1_c = image_1_a + image_1_b
+dft_image_1_c = np.fft.fftshift(image_1_c)
+magnitude_dft_image_1_c = np.abs(dft_image_1_c)
+
+imagesToPlot = [image_1_c, np.log(magnitude_dft_image_1_c + 1)]
+label = ["sinusoidal vertical + horizontal", "magnitude"]
+
+lib.affichage_rows_cols_images(1, 2, imagesToPlot, label)
+
+# (d)
+
+image_1_d = transform.rotate(image_1_c, 25)
+wm, hm = lib.rotatedRectWithMaxArea(image_1_c.shape[0], image_1_c.shape[1], lib.math.radians(25))
+image_1_d = lib.crop_around_center(image_1_d, wm, hm)
+dft_image_1_d = np.fft.fftshift(image_1_d)
+magnitude_dft_image_1_d = np.abs(dft_image_1_d)
+
+imagesToPlot = [image_1_d, np.log(magnitude_dft_image_1_d + 1)]
+label = ["sinusoidal vertical + horizontal - rotated and cropped", "magnitude"]
+
+lib.affichage_rows_cols_images(1, 2, imagesToPlot, label)
+
+# (e)
+
+image_1_e = image_1_a * image_1_b
+dft_image_1_e = np.fft.fftshift(image_1_e)
+magnitude_dft_image_1_e = np.abs(dft_image_1_e)
+
+imagesToPlot = [image_1_e, np.log(magnitude_dft_image_1_e + 1)]
+label = ["sinusoidal vertical * horizontal", "magnitude"]
+
+lib.affichage_rows_cols_images(1, 2, imagesToPlot, label)
---
+++ imagerieNumerique/TP_11/lib.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import math
+
+
+def affichage_rows_cols_images(rows, cols, images, labels):
+    rows = rows
+    cols = cols
+    axes = []
+    fig = plt.figure()
+    for i in range(rows * cols):
+        axes.append(fig.add_subplot(rows, cols, i + 1))
+        plt.imshow(images[i], cmap='gray')
+        plt.title(labels[i])
+    fig.tight_layout()
+    plt.show()
+
+
+def rotatedRectWithMaxArea(w, h, angle):
+    """
+  Given a rectangle of size wxh that has been rotated by 'angle' (in
+  radians), computes the width and height of the largest possible
+  axis-aligned rectangle (maximal area) within the rotated rectangle.
+  """
+    if w <= 0 or h <= 0:
+        return 0, 0
+
+    width_is_longer = w >= h
+    side_long, side_short = (w, h) if width_is_longer else (h, w)
+
+    # since the solutions for angle, -angle and 180-angle are all the same,
+    # if suffices to look at the first quadrant and the absolute values of sin,cos:
+    sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
+    if side_short <= 2. * sin_a * cos_a * side_long or abs(sin_a - cos_a) < 1e-10:
+        # half constrained case: two crop corners touch the longer side,
+        #   the other two corners are on the mid-line parallel to the longer line
+        x = 0.5 * side_short
+        wr, hr = (x / sin_a, x / cos_a) if width_is_longer else (x / cos_a, x / sin_a)
+    else:
+        # fully constrained case: crop touches all 4 sides
+        cos_2a = cos_a * cos_a - sin_a * sin_a
+        wr, hr = (w * cos_a - h * sin_a) / cos_2a, (h * cos_a - w * sin_a) / cos_2a
+
+    return wr, hr
+
+
+def crop_around_center(image, width, height):
+    """
+    Given a NumPy / OpenCV 2 image, crops it to the given width and height,
+    around it's centre point
+    """
+
+    image_size = (image.shape[1], image.shape[0])
+    image_center = (int(image_size[0] * 0.5), int(image_size[1] * 0.5))
+
+    if width > image_size[0]:
+        width = image_size[0]
+
+    if height > image_size[1]:
+        height = image_size[1]
+
+    x1 = int(image_center[0] - width * 0.5)
+    x2 = int(image_center[0] + width * 0.5)
+    y1 = int(image_center[1] - height * 0.5)
+    y2 = int(image_center[1] + height * 0.5)
+
+    return image[y1:y2, x1:x2]
