83a75a29ce6d6a241cba90823e5abc7343668e5b
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2021-06-05 00:41:12 +0200

    s

--- imagerieNumerique/TP_12/t.py
+++ imagerieNumerique/TP_12/t.py
@@ -1,40 +1,20 @@
-import lib
-import numpy as np
-import skimage.util as util
-
-# a
-
-path = "lena.png"
-imageLena = lib.loadImag(path)
-
-filter = 1 / 9 * lib.np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-image_padded = lib.padding(imageLena, filter, 'constant')
-image_padded_filtered = lib.image_filtering(image_padded, filter)
-image_padded_filtered_a = lib.get_cropped_image(image_padded_filtered, -1)
-
-mseFilterA = lib.metric.mean_squared_error(imageLena, image_padded_filtered_a)
-
-
-# b
-
-ideal_lowpass_filter = lib.idealLowPassFilter(image_padded_filtered_a, 600)
-mse_ideal = lib.metric.mean_squared_error(imageLena, ideal_lowpass_filter)
+def t(x):
+    print()
+    if x == -1:
+        print("x = -1",1)
+        return 1
+    elif x == 0:
+        print("x = 0",0)
+        return 0
+    elif x == 1:
+        print("x = 1",2)
+        return 2
+    else:
+        temp = t(x - 3) / (x * x - 1)
+        print("ici - x = ", x ," ",temp)
+        return temp
 
 
-butter_lowpass_filter = lib.butterLowPassFilter(image_padded_filtered_a, 200, 2)
-mse_butter = lib.metric.mean_squared_error(imageLena, butter_lowpass_filter)
+l = t(5)
 
-
-gaussian_lowpass_filter = lib.gaussianLowPassFilter(image_padded_filtered_a, 200)
-mse_gaussian = lib.metric.mean_squared_error(imageLena, gaussian_lowpass_filter)
-
-
-imagesToPlot = [imageLena, image_padded_filtered_a, image_padded_filtered, ideal_lowpass_filter, butter_lowpass_filter, gaussian_lowpass_filter]
-label = ["image lena", "box blur lena", "gaussian filter lena (a)", "ideal high pass (b)", "butter high pass (b)", "gaussian high pass (b)"]
-lib.affichage_rows_cols_images(2, 3, imagesToPlot, label)
-
-print("MSE -> lena & lena noisy -> " + str(mseFilterA))
-print("MSE -> lena & ideal high pass (b) - cutoff = 200 -> " + str(mse_ideal))
-print("MSE -> lena & butter high pass (b) - cutoff = 200 - n = 2 -> " + str(mse_butter))
-print("MSE -> lena & gaussian high pass (b) - cutoff = 200 -> " + str(mse_gaussian))
-
+print(" l = ", l)
